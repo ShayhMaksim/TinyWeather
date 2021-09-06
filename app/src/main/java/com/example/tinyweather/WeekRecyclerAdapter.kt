c67877c0ddff4d817af12b594385ad9d721a2910
ShayhMaksim <sargaras_m@mail.ru>
2021-09-06 23:18:30 +0300

    Update RecyclerView

--- app/src/main/java/com/example/tinyweather/WeekRecyclerAdapter.kt
+++ app/src/main/java/com/example/tinyweather/WeekRecyclerAdapter.kt
@@ -1,11 +1,19 @@
 package com.example.tinyweather
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.lang.StrictMath.round
+import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -15,10 +23,12 @@ class WeekRecyclerAdapter(private val names: List<WeatherJSON.Daily>) :
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var textViewDay: TextView? = null
         var textViewTemp: TextView? = null
+        var imageViewDay: ImageView? = null
 
         init {
             textViewDay = itemView.findViewById(R.id.textViewDay)
             textViewTemp = itemView.findViewById(R.id.textViewTemp)
+            imageViewDay = itemView.findViewById(R.id.imageViewDay)
         }
     }
 
@@ -36,6 +46,21 @@ class WeekRecyclerAdapter(private val names: List<WeatherJSON.Daily>) :
 
         holder.textViewDay?.text = "${sdfDay.format(date)} ${monthNamesRussia[sdfMonth.format(date).toInt()]}\n"
         holder.textViewTemp?.text = "Температура на улице ${round(names[position].temp.day-273)}"
+
+        runBlocking {
+            launch(Dispatchers.IO) {
+                var image: Bitmap? = null
+                try {
+                    val `in` = URL("https://openweathermap.org/img/wn/${names[position].weather[0].icon}@2x.png").openStream()
+                    image = BitmapFactory.decodeStream(`in`)
+                    holder.imageViewDay?.setImageBitmap(image)
+                } catch (e: Exception) {
+                    Log.e("Error Message", e.message.toString())
+                    e.printStackTrace()
+                }
+            }
+        }
+
     }
 
     override fun getItemCount(): Int {
